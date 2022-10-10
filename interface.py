e7a9722da897c423fa4a9374f8426f6e8ea57064
Alan Gomez <203429@ids.upchiapas.edu.mx>
2022-10-10 02:21:50 -0500

    interface updates

---
+++ interface.py
@@ -0,0 +1,91 @@
+from tkinter import *
+from tkinter import filedialog
+import main as m
+
+tf_validate = False
+
+def open_file():
+    tf = open("./Reporte.txt")
+    data = tf.read()
+    txtarea.delete(1.0,END)
+    txtarea.insert(END, data)
+    tf.close()
+
+def open_dir():
+    global dirr;
+    dirr = filedialog.askdirectory(title='Abrir carpeta ...', initialdir='/')
+    pathh.insert(END, dirr)
+
+def script(dirr):
+    m.main(dirr)
+    open_file()
+
+
+ws = Tk()
+ws.title("Validador de importaciones")
+ws.geometry("600x650")
+ws['bg']='#C1C1C1'
+
+txtarea = Text(ws, width=65, height=25)
+txtarea.pack(pady=30)
+
+pathh = Entry(ws)
+pathh.pack(side=LEFT, expand=True, fill=X, padx=10)
+
+dir_button = Button(
+    ws, 
+    text="Buscar Carpeta", 
+    command=open_dir
+    ).pack(side=RIGHT, expand=True, fill=X, padx=10)
+
+val_button = Button(
+    ws, 
+    text="Validar", 
+    command=lambda: script(dirr)
+    ).pack(side=RIGHT, expand=True, fill=X, padx=10)
+
+ws.mainloop()
+# import tkinter as tk
+# import tkinter.filedialog as fd
+# from tkinter.ttk import LabelFrame
+# from tkinter import *
+# import main as m
+
+# is_packed = False
+
+# class Application(tk.Tk):
+#     def __init__(self):
+#         super().__init__()
+#         self.inicializar_gui()
+
+#     def inicializar_gui(self):
+#         self.title('Validador de importaciones')
+#         self.geometry('1000x400')
+
+#         txt_directory = tk.Label(self, text="Ruta del directorio: ")
+#         txt_directory.pack(padx=60, pady=30)
+#         txt_path = tk.Label(self, text="")
+#         txt_path.pack(padx=60, pady=30)
+
+#         btn_seleccionar_carpeta = tk.Button(self, text='Seleccionar carpeta...')
+#         btn_seleccionar_carpeta['command'] = lambda: self.seleccionar_carpeta(txt_path)
+#         btn_seleccionar_carpeta.pack(padx=60, pady=10)
+
+#     def seleccionar_carpeta(self,txt_path):
+#         global directorio;
+#         global is_packed
+#         directorio = fd.askdirectory(title='Abrir carpeta ...', initialdir='/')
+#         if directorio:
+#             txt_path.config(text = directorio)
+#             btn_validar = tk.Button(self, text='Validar')
+#             btn_validar['command'] = lambda: m.main(directorio)
+#             if is_packed==False:
+#                 is_packed=True
+#                 btn_validar.pack(padx=60, pady=10)
+
+# def main():
+#     app = Application()
+#     app.mainloop()
+
+# if __name__ == '__main__':
+#     main()
--- main.py
+++ main.py
@@ -1,28 +1,43 @@
+from importlib.resources import path
 import transitions as dfa
 import os
 
 validated_lines = []
 path_items = []
 
-def get_paths():
-    path = ".\examples"
+def restart_lists():
+    global validated_lines
+    global path_items
+    validated_lines = []
+    path_items = []
+
+def get_paths(path):
     for dir, subdir, files in os.walk(path):
         for file in files:
             filepath = os.path.join(dir, file)
-            validated_lines.append(filepath)
+            # validated_lines.append(filepath)
             read_file(filepath)
             validated_lines.append("<>")
 
 def read_file(filepath):
+    conta = 0
     with open(filepath) as fp:
         line = fp.readline()
         while line:
-            validate_automata(line.strip())
+            is_valid = validate_automata(line.strip())
+            if is_valid==True:
+                if conta == 0:
+                    validated_lines.append(filepath)
+                    conta = 1
+                validated_lines.append(line.strip())
             line = fp.readline()
 
 def validate_automata(text):
     if dfa.dfa.accepts_input(text):
-        validated_lines.append(text)
+        # validated_lines.append(text)
+        return True
+    else:
+        return False
 
 def separate_list():
     temp = []
@@ -34,7 +49,7 @@ def separate_list():
             temp = []
 
 def write_txt():
-    f = open("Reporte.txt","a+")
+    f = open("Reporte.txt","w+")
     for x in path_items:
         for j in range(len(x)):
             if j==0:
@@ -55,9 +70,11 @@ def write_txt():
                     # f.write(a)
                     f.write("\n")
                     # f.write(x[j] + "\n")
-    f.write("-----------------------------------------------------------------------------------")
+        f.write("\n")
     f.close()
 
-get_paths()
-separate_list()
-write_txt()
+def main(path):
+    restart_lists()
+    get_paths(path)
+    separate_list()
+    write_txt()
